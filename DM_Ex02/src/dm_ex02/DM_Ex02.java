7f7ee7e6fa1e02aed2669decfa0784bfc94b7071
Athan Bonis <athancb@hotmail.com>
2018-05-30 23:06:32 +0300

    Ex02 Started

---
+++ DM_Ex02/src/dm_ex02/DM_Ex02.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dm_ex02;
+
+/**
+ *
+ * @author AthanasiosBonis
+ */
+public class DM_Ex02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame mf = new MainFrame();
+        mf.show();
+    }
+    
+}
